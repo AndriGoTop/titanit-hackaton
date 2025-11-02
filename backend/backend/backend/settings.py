0b9b5e9290e4cf0f1e989cb63e6f961ae860c479
AndriGoTop <a.golubn@gmail.com>
2025-11-03 00:22:06 +0300

    исправил баг

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -67,11 +67,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 ELASTICSEARCH_DSL = {
     'default': {
-<<<<<<< HEAD
-        'hosts': 'http://elasticsearch:9200',
-=======
         'hosts': os.getenv("ELASTICSEARCH_DSL", 'http://localhost:9200'),
->>>>>>> b937d4bd878d2b1a08dc2d367fa762944aba917b
     },
 }
 ELASTICSEARCH_DSL_SIGNAL_PROCESSOR = 'django_elasticsearch_dsl.signals.RealTimeSignalProcessor'
